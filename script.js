fdcf5bf506204bfa5d40e6ee33cab33f6c73d246
stolluka <stolzlukas@gmx.net>
2026-02-09 16:31:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-const headers = document.querySelectorAll(".accordion-header");
 
-headers.forEach(header => {
-    header.addEventListener("click", () => {
-        const content = header.nextElementSibling;
-
-        if (content.style.maxHeight) {
-            content.style.maxHeight = null;
-        } else {
-            content.style.maxHeight = content.scrollHeight + "px";
-        }
-    });
-});
